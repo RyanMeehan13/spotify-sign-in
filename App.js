24b416221ddff3a224ede6252373b99644d90ba9
Ryan Meehan <ryan.meehan200213@gmail.com>
2022-11-07 11:51:01 -0500

    Working sign in for spotify

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//Sample code from expo docs explaining how to set up spotify login. 
+
+
+import * as React from 'react';
+import * as WebBrowser from 'expo-web-browser';
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
+import { Button, View, Text, StyleSheet } from 'react-native';
+
+WebBrowser.maybeCompleteAuthSession();
+
+// Endpoint
+const discovery = {
+  authorizationEndpoint: 'https://accounts.spotify.com/authorize',
+  tokenEndpoint: 'https://accounts.spotify.com/api/token',
+};
 
 export default function App() {
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: 'PASTE CLIENT ID HERE', //my unique client id
+      scopes: ['user-read-email', 'playlist-modify-public'],
+      // In order to follow the "Authorization Code Flow" to fetch token after authorizationEndpoint
+      // this must be set to false
+      usePKCE: false, //--above comments say this should be false, but spotify docs recommend using PKCE for mobile - idk
+      redirectUri: makeRedirectUri({
+        scheme: 'https://localhost:19006',
+        path: "gdtlogin/",
+      }),
+    },
+    discovery
+  );
+
+  React.useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params;
+      //console.log('Response = success!');
+      alert("You've successfully signed into Spotify");
+    }
+  }, [response]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  
+<View style = {styles.container}>
+    <Button
+    style = {styles.button}
+      disabled={!request}
+      title="Login"
+      onPress={() => {
+        promptAsync();
+      }}
+    />
+    <Text>Press the button to login using Spotify!</Text>
     </View>
   );
 }
@@ -16,5 +60,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 10
   },
-});
+  button: {
+    alignItems: "center",
+
+    padding: 10
+  },
+})
